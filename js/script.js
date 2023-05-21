84c902b2abfe106777cde82ec321e6582848b91d
Patryk Zwierzyk <z.patryk988@gmail.com>
2023-05-21 18:53:05 +0200

    initlial commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+{
+    const tasks = [
+        {
+            content: "nagrać lekscje",
+            done: false,
+        },
+        {
+            content: "cokolwiek",
+            done: true,
+        },
+
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+        <li>
+         ${task.content}
+        </li>
+        `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+
+            const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+            if (newTaskContent === "") {
+                return;
+            }
+
+            tasks.push({
+                content: newTaskContent,
+            });
+
+            render();
+        });
+    };
+
+    init();
+}
