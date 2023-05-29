0ac6a6857f6d9c37fc42deef2f7383a516f984e0
Patryk Zwierzyk <z.patryk988@gmail.com>
2023-05-29 18:31:32 +0200

    remove tasks

--- js/script.js
+++ js/script.js
@@ -1,26 +1,40 @@
 {
     const tasks = [];
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-        <li>
-         ${task.content}
-        </li>
-        `;
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
         });
 
         render();
+    };
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    } 
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+                ${task.content}  
+                <button class="js-remove">usuń</button>
+            </li>
+           `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
     };
 
     const onFormSubmit = (event) => {
