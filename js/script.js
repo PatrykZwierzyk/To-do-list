a78160b6d486b76838a2320a0a110b64d47bf869
Patryk Zwierzyk <z.patryk988@gmail.com>
2023-07-08 11:12:01 +0200

    refactor according to immutability

--- js/script.js
+++ js/script.js
@@ -1,23 +1,31 @@
 {
-    const tasks = [];
+    let tasks = [];
 
     const addNewTask = (newTaskContent) => {
-        tasks.push({
-            content: newTaskContent,
-        });
+        tasks = [
+            ...tasks,
+            { content: newTaskContent }
+        ];
 
         render();
     }
 
     const removeTask = (index) => {
-        tasks.splice(index, 1);
+        tasks = [
+            ...tasks.slice(0, index),
+            ...tasks.slice(index + 1)
+        ];
         render();
     }
 
     const toggleTaskDone = (index) => {
-        tasks[index].done = !tasks[index].done;
+        tasks = [
+          ...tasks.slice(0, index),
+          { ...tasks[index], done: !tasks[index].done },
+          ...tasks.slice(index + 1)
+        ];
         render();
-    }
+      }
 
     const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
